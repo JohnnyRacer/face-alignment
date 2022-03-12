d2cb1d24a708b27b5253c978a0f6d7f6e73e347f
JohnnyRacer <77214388+JohnnyRacer@users.noreply.github.com>
2022-03-12 15:00:01 -0800

    Update core.py

--- face_alignment/detection/core.py
+++ face_alignment/detection/core.py
@@ -18,17 +18,17 @@ class FaceDetector(object):
     def __init__(self, device, verbose):
         self.device = device
         self.verbose = verbose
-"""
+
         if verbose:
             if 'cpu' in device:
                 logger = logging.getLogger(__name__)
                 logger.warning("Detection running on CPU, this may be potentially slow.")
 
-        if 'cpu' not in device and 'cuda' and 'xla' not in device:
+        if 'cpu' not in device and 'cuda' not in device:
             if verbose:
-                logger.error("Expected values for device are: {cpu, cuda, xla} but got: %s", device)
+                logger.error("Expected values for device are: {cpu, cuda} but got: %s", device)
             raise ValueError
-"""
+
     def detect_from_image(self, tensor_or_path):
         """Detects faces in a given image.
 
